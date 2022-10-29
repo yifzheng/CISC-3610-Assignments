b4bcc64454585cb4e2c3588768f555f2d62e12b4
Yifeng Zheng <yifeng.zheng43@bcmail.cuny.edu>
2022-10-28 20:10:05 -0400

    finished audio lab

---
+++ AudioLab/js/index.js
@@ -0,0 +1,24 @@
+const btnOne = () => {
+    let audio = document.getElementById("evil-laugh");
+    audio.play();
+};
+
+const btnTwo = () => {
+    let aud = document.getElementById("evil-shreik");
+    aud.play();
+};
+
+const btnThree = () => {
+    let aud = document.getElementById("scream-with-echo");
+    aud.play();
+};
+
+const btnFour = () => {
+    let aud = document.getElementById("possessed-laugh");
+    aud.play();
+};
+
+const btnFive = () => {
+    let aud = document.getElementById("ghost-whispers");
+    aud.play();
+};
